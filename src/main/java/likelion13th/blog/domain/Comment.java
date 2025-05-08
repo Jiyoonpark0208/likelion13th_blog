0a3d17905fa1367aed7cb32078585772ba4e94b4
Jiyoonpark0208 <ascii0208@naver.com>
2025-05-08 21:00:39 +0900

    Feat:게시글 생성, ë게시글 전체 êµ조회 기능 구현

---
+++ src/main/java/likelion13th/blog/domain/Comment.java
@@ -0,0 +1,48 @@
+package likelion13th.blog.domain;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+
+    private Long id;
+    private Article article;
+    private String content;
+    private String author;
+    private String password;
+    private LocalDateTime createdAt;
+
+
+    public Comment(Long id, String author, String password, Article article, String content) {
+
+        this.id = id;
+        this.author = author;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+        this.content = content;
+        this.article=article;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Long getId() {return id; }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPassword() {return password;}
+}
+
+
+
+
