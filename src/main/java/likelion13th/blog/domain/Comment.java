a850b5d262b57c15eb830aeae0b3494afae6c793
Jiyoonpark0208 <ascii0208@naver.com>
2025-05-26 20:25:39 +0900

    Feat: 댓글 domain 코드 추가

--- src/main/java/likelion13th/blog/domain/Comment.java
+++ src/main/java/likelion13th/blog/domain/Comment.java
@@ -1,50 +1,39 @@
-/*
 package likelion13th.blog.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="article_id", nullable = false)
+    @OnDelete(action= OnDeleteAction.CASCADE)
     private Article article;
+
+    @Column(nullable = false)
     private String content;
+
+    @Column(nullable = false)
     private String author;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
-
-    public Comment(Long id, String author, String password, Article article, String content) {
-
-        this.id = id;
-        this.author = author;
-        this.password = password;
-        this.createdAt = LocalDateTime.now();
-        this.content = content;
-        this.article=article;
-    }
-
-    public Article getArticle() {
-        return article;
-    }
-    public String getContent() {
-        return content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public Long getId() {return id; }
-
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getPassword() {return password;}
 }
-
-
-
-
-*/
